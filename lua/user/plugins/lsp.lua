3e927654b65d560986dd761131d8045344fc7b6f
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2025-01-15 11:45:51 -0500

    v0.1.3

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -18,6 +18,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local opts = {buffer = event.buf}
 
+    vim.keymap.set('n', 'ls', '<CMD>Telescope diagnostics<CR>', opts)
+    vim.keymap.set('n', 'lf', '<CMD>lua vim.lsp.buf.format()<CR>', opts)
+    vim.keymap.set('n', 'lr', '<CMD>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.keymap.set('n', 'la', '<CMD>lua vim.lsp.buf.code_action()<CR>', opts)
+
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
@@ -25,9 +30,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
     vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   end,
 })
 
